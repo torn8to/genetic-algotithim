28af9c74a2a6272fe67fac3c53525b924f6f0107
Nathan Rogers <nathanroger314@gmail.com>
2022-02-12 21:03:45 -0500

    worrking genetic algorithim main method demonstates how it runds more changes later

--- src/tower_stacking.py
+++ src/tower_stacking.py
@@ -12,9 +12,9 @@ class floor_type(Enum):
 class tower_floor:
     def __init__(self,floor_type:str, width:int,strength:int,cost:int):
         self.floor_type = self.floor_type_enumeration(floor_type)
-        self.width = width
-        self.strength = strength
-        self.cost = cost
+        self.width = int(width)
+        self.strength = int(strength)
+        self.cost = int(cost)
 
     def __eq__(self, other):
         return self.floor_type == other.floor_type and \
@@ -62,9 +62,15 @@ class tower:
             return False
         elif  self.multiples_of_floor_types(floor_type.door) or self.multiples_of_floor_types(floor_type.lookout):
             return False
-        for x in range(1,len(self.floor_structure)):
+        for x in range(1,len(self.floor_structure)-1):
             if self.floor_structure[x].width > self.floor_structure[x-1].width:
                 return False
+        return True
+    def get_floors(self):
+        return self.floor_structure
+
+    def set_floors(self,struct):
+        self.floor_structure = struct
 
     def get_cost(self):
         sum = 0
@@ -78,8 +84,7 @@ class tower:
     def evaluate(self) -> int:
         if not self.valid_tower():
             return 0
-        elif True:
-            return self.score()
+        return self.score()
 
 
 
@@ -113,11 +118,13 @@ class culling_method(Enum):
 
 
 class tower_stacker_genetics():
-    def __init__(self,file:open, population = 1000 ):
+    def __init__(self,file:open, population = 1000, elitism = False, culling = False):
         self.options = data_pool(self.process_data(file))
         self.population_cap= population
         self.all_scores = []
         self.generation = 0
+        self.elitism = elitism
+        self.culling = culling
 
 
 
@@ -137,6 +144,7 @@ class tower_stacker_genetics():
     def  random_generation(self):
         self.pool = []
         self.previous_generations = []
+        self.previous_scores = []
         self.pool_scores = []
         for x in range(self.population_cap):
             self.pool.append(self.options.generate_random_tower())
@@ -144,11 +152,10 @@ class tower_stacker_genetics():
 
     def score_generation(self):
         for x in self.pool:
-            print(1)
             self.pool_scores.append(x.evaluate())
         zipped = zip(self.pool_scores,self.pool)
-        zipped.sort()
-        tuples = zip(sorted_pairs)
+        sorted_pairs = sorted(zipped, key  = lambda pair:pair[0],reverse=True)
+        tuples = zip(*sorted_pairs)
         self.pool_scores, self.pool = [list(tuple) for tuple in tuples]
         max = self.pool_scores[0]
         min = self.pool_scores[self.population_cap-1]
@@ -159,13 +166,15 @@ class tower_stacker_genetics():
 
     def crossover(self,first, second):
         max_cross_spot = len(second) - 2
-        if len(first.floor) < len(second):
+        if len(first) < len(second):
              max_cross_spot = len(first)
+        cross_spot = 1
+        if max_cross_spot == 1:
+            cross_spot = random.randint(1,max_cross_spot)
 
-        cross_spot = random.randint(1,max_cross_spot)
         placeholder = first[cross_spot:]
         first[cross_spot:] = second[cross_spot:]
-        second[cross_spot:] = first[cross_spot]
+        second[cross_spot:] = placeholder
         return first , second
 
 
@@ -185,20 +194,26 @@ class tower_stacker_genetics():
         else :
             crossover_pool = self.pool
 
-        while (new_generation < self.population_cap):
-            first_random_index = random.randint(0,len(crossover_pool))
-            second_random_index = random.randint(0,len(crossover_pool))
+        while (len(new_generation) < self.population_cap):
+            first_random_index = random.randint(0,len(crossover_pool)-1)
+            second_random_index = random.randint(0,len(crossover_pool)-1)
             if second_random_index == first_random_index:
                 second_random_index +=1
             if second_random_index == first_random_index and second_random_index >len(self.population_cap)-1:
                 second_random_index -=2
-            f = crossover_pool[first_random_index]
-            s = crossover_pool[second_random_index]
-            f,s = crossover_pool(f.floor_Structure,s.floor_structure)
+            f, s = crossover_pool[first_random_index], crossover_pool[second_random_index]
+            child_1, child_2= self.crossover(f.get_floors(),s.get_floors())
+            f.set_floors(child_1)
+            s.set_floors(child_2)
+
             new_generation.append(f)
             new_generation.append(s)
+
+        print(len(new_generation))
+        L = tower([tower_floor("door", 5, 5, 1), tower_floor("lookout",4,3,1)])
+        print(L.evaluate())
         self.pool = new_generation
-        self.pool_scores = new_generation
+        self.pool_scores = []
 
 
     def run_for_n_time(self, max_run_time):
@@ -207,6 +222,7 @@ class tower_stacker_genetics():
         self.random_generation()
         while (current_time - begin).total_seconds() < 5.0:
             self.create_new_generation()
+            print(self.previous_scores[len(self.previous_scores)-1])
             current_time = datetime.now()
         #TODO print a chart with median min and max data
         #be able to export data to csv
@@ -216,5 +232,4 @@ class tower_stacker_genetics():
 if __name__ == "__main__":
     f = open(r'C:\Users\nathan\PycharmProjects\genetic\genetic-algotithim\src\tower_example.txt',"r")
     t = tower_stacker_genetics(f)
-    t.random_generation()
-    t.create_new_generation()
+    t.run_for_n_time(5)
